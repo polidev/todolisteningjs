e8e188564b0a3cc6341dfa0c7eb8ee0618fc2167
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-06-14 12:13:52 -0600

    add: music player

---
+++ src/components/musicPlayer/musicPlayer.jsx
@@ -0,0 +1,14 @@
+export default function MusicPlayer() {
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/X4VbdwhkE10?si=XSLm9ICL6ALsmpf7"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    ></iframe>
+  );
+}
