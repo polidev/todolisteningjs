a4b21839c35520785ee7d11aa970930a251d44e1
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-06-14 12:24:27 -0600

    add: shop list custom hook

---
+++ src/hooks/useShopList.jsx
@@ -0,0 +1,55 @@
+import useLocalStorage from "./useLocalStorage.jsx";
+
+export default function useShopList() {
+  const [shopList, setShopList] = useLocalStorage("shopList", []);
+
+  function addShopTodo(text) {
+    setShopList((prev) => [
+      ...prev,
+      { id: crypto.randomUUID(), text, completed: false, quantity: 1 },
+    ]);
+  }
+
+  function toggleShopTodo(id) {
+    setShopList((prev) =>
+      prev.map((shopTodo) =>
+        shopTodo.id === id
+          ? { ...shopTodo, completed: !shopTodo.completed }
+          : shopTodo,
+      ),
+    );
+  }
+
+  function deleteShopTodo(id) {
+    setShopList((prev) => prev.filter((shopTodo) => shopTodo.id !== id));
+  }
+
+  function incrementQuantity(id) {
+    setShopList((prev) =>
+      prev.map((shopTodo) =>
+        shopTodo.id === id
+          ? { ...shopTodo, quantity: shopTodo.quantity + 1 }
+          : shopTodo,
+      ),
+    );
+  }
+
+  function decrementQuantity(id) {
+    setShopList((prev) =>
+      prev.map((shopTodo) =>
+        shopTodo.id === id
+          ? { ...shopTodo, quantity: shopTodo.quantity - 1 }
+          : shopTodo,
+      ),
+    );
+  }
+
+  return {
+    shopList,
+    addShopTodo,
+    toggleShopTodo,
+    deleteShopTodo,
+    incrementQuantity,
+    decrementQuantity,
+  };
+}
