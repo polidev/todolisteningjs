987cd5337176949a4abe863695c793df745349d5
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-06-14 09:33:56 -0600

    feat: add, complete, delete and count todos

---
+++ src/components/todoItem/todoItem.jsx
@@ -0,0 +1,20 @@
+import "./todoItem.css";
+
+export default function TodoItem({ todo, toggleTodo, deleteTodo }) {
+  return (
+    <>
+      <div className="todo-item">
+        <input
+          type="checkbox"
+          id={`taskCheckbox-${todo.id}`}
+          checked={todo.completed}
+          onChange={() => toggleTodo(todo.id)}
+        />
+        <label htmlFor={`taskCheckbox-${todo.id}`} id={`taskLabel-${todo.id}`}>
+          {todo.text}
+        </label>
+        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+      </div>
+    </>
+  );
+}
--- src/pages/todoPage.jsx
+++ src/pages/todoPage.jsx
@@ -1,7 +1,34 @@
+import TodoInput from "../components/todoInput/todoInput.jsx";
+import TodoItem from "../components/todoItem/todoItem.jsx";
+import useTodos from "../hooks/useTodos.jsx";
+
 export default function TodoPage() {
+  const { todos, addTodo, toggleTodo, deleteTodo } = useTodos();
+
+  const completed = todos.filter((t) => t.completed).length;
+  const total = todos.length;
+
   return (
-    <div>
-      <h1>Todo Page</h1>
-    </div>
+    <>
+      <TodoInput addTodo={addTodo} />
+      <section>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </section>
+
+      <section>
+        {total > 0 && (
+          <p>
+            {completed} of {total} tasks complete
+          </p>
+        )}
+      </section>
+    </>
   );
 }
