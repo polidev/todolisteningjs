22446349aae8076fcee023b92b4ad7f19173374e
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-06-20 12:22:25 -0600

    style: error page

--- src/pages/errorPage.jsx
+++ src/pages/errorPage.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router";
+import "./errorPage.css";
+
 export default function ErrorPage() {
   return (
     <div className="error-page">
       <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
+      <Link to="/">Go back to Home</Link>
     </div>
   );
 }
